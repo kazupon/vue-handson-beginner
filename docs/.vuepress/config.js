c52db1302194c4d965aeeb676268eea6ab05de36
kazuya kawaguchi <kawakazu80@gmail.com>
2018-04-15 16:00:46 +0900

    :pencil: docs: scaffloding

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Vue Hands-on',
+  description: 'Vue.js Hands-on for Beginner',
+  themeConfig: {
+    repo: 'kazupon/vue-handson-beginner',
+    docsDir: 'docs',
+    nav: [{
+      text: 'イントロダクション',
+      link: '/intro/',
+    }, {
+      text: 'セットアップ',
+      link: '/setup/'
+    }, {
+      text: 'ハンズオン',
+      link: '/handson/'
+    }],
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+heroImage: /logo.png
+actionText: イントロダクション →
+actionLink: /intro/
+footer: MIT Licensed | Copyright © 2018 kazuya kawaguchi
+---
+
+### :warning: 注意事項
+このリポジトリは、下記ハンズオンのためのものです。
+
+- [Vue.js初心者向けハンズオン](https://vuejs-meetup.connpass.com/event/74894/)
